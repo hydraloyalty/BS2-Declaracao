e2df7799dd09457517ae33b16aab222e222628c4
Hydra <github@equipehydra.com>
2024-06-05 15:35:06 -0300

    Initial commit

---
+++ Scripts/titulo.js
@@ -0,0 +1,4 @@
+document.getElementById('i-rs').addEventListener('input', function() {
+            var rs = this.value;
+            document.title = "Declaração de Quitação de Pendência [BANCO BS2 e BACEN] - " + rs;
+        });
